4f23497bc864aba8a7b04075296fc88d69b491cb
OrcDev <urosmiric88@gmail.com>
2025-07-20 22:48:55 +0200

    feat(sidebar): wrap logo in a link to navigate to the dashboard

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -11,6 +11,7 @@ import {
 import { getNotebooks } from "@/server/notebooks";
 import Image from "next/image";
 import { SidebarData } from "./sidebar-data";
+import Link from "next/link";
 
 export async function AppSidebar({
   ...props
@@ -34,10 +35,10 @@ export async function AppSidebar({
   return (
     <Sidebar {...props}>
       <SidebarHeader>
-        <div className="flex items-center gap-2">
+        <Link href="/dashboard" className="flex items-center gap-2 pl-2">
           <Image src="/noteforge-logo.png" alt="Logo" width={32} height={32} />
           <h2>NoteForge</h2>
-        </div>
+        </Link>
 
         <React.Suspense>
           <SearchForm />
